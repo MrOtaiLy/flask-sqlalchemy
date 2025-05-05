5ed4f7789c1fe6a919a5b1b5c04a7dfbf68c9bc3
MrOtaiLy <lykovvana824@gmail.com>
2025-05-05 12:03:00 +0300

    Форма регистрации

--- FlaskApp.py
+++ FlaskApp.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from data.db_session import global_init, create_session
 from data.jobs import Jobs
+from data.users import User
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 
@@ -10,6 +12,34 @@ def jobs_list():
     session = create_session()
     jobs = session.query(Jobs).all()
     return render_template("jobs_list.html", jobs=jobs)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        if request.form["password"] != request.form["repeat_password"]:
+            return render_template("register.html", message="Пароли не совпадают")
+
+        session = create_session()
+        if session.query(User).filter(User.email == request.form["email"]).first():
+            return render_template(
+                "register.html", message="Такой пользователь уже есть"
+            )
+
+        user = User(
+            surname=request.form["surname"],
+            name=request.form["name"],
+            age=int(request.form["age"]),
+            position=request.form["position"],
+            speciality=request.form["speciality"],
+            address=request.form["address"],
+            email=request.form["email"],
+            hashed_password=generate_password_hash(request.form["password"]),
+        )
+        session.add(user)
+        session.commit()
+        return redirect("/jobs")
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
